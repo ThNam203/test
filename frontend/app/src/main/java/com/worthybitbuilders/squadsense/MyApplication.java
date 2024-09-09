68949aa914778e9eacda0386a3a199ef19148301
sen1or <hthnam203@gmail.com>
2024-09-09 14:13:38 +0000

    khoi commit

--- frontend/app/src/main/java/com/worthybitbuilders/squadsense/MyApplication.java
+++ frontend/app/src/main/java/com/worthybitbuilders/squadsense/MyApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.worthybitbuilders.squadsense.utils.SharedPreferencesManager;
 
 public class MyApplication extends Application {
+    String hello;
     @Override
     public void onCreate() {
         super.onCreate();
