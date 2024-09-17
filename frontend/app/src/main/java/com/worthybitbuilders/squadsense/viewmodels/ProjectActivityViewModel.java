a3632ca3b51649a195f61a27f1a2f0fdc1ce7916
sen1or <hthnam203@gmail.com>
2024-09-17 03:13:00 +0000

    fix missing methods after merging

--- frontend/app/src/main/java/com/worthybitbuilders/squadsense/viewmodels/ProjectActivityViewModel.java
+++ frontend/app/src/main/java/com/worthybitbuilders/squadsense/viewmodels/ProjectActivityViewModel.java
@@ -159,7 +159,7 @@ public class ProjectActivityViewModel extends ViewModel {
 
     public void removeProject(String projectId, ApiCallHandlers handlers) {
         String userId = SharedPreferencesManager.getData(SharedPreferencesManager.KEYS.USER_ID);
-        Call<Void> call = projectService.deleteProject(userId, projectId);
+        Call<Void> call = projectService.removeProject(userId, projectId);
         call.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
