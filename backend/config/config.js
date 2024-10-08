4ab7c91b12188612701e0b7b5b8ac0a71085f7f7
ThNam203 <hthnam203@gmail.com>
2024-10-08 19:38:15 +0700

    upgrade project sdk

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,12 +3,11 @@ const fs = require('fs')
 
 // instead of the product_config which is hidden because of security,
 // use dev_config.env file at your own risk
-if (process.env.NODE_ENV === 'development') {
-    const prodConfigPath = __dirname.concat('/product_config.env')
-    if (fs.existsSync(prodConfigPath))
-        dotenv.config({ path: __dirname.concat('/product_config.env') })
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) {
+    const devConfigPath = __dirname.concat('/.env')
+    if (fs.existsSync(devConfigPath))
+        dotenv.config({ path: devConfigPath })
     else {
-        dotenv.config({ path: __dirname.concat('/dev_config.env') })
-        console.log("please work please")
+        dotenv.config({ path: __dirname.concat('/product.env') })
     }
 }
--- frontend/app/src/main/java/com/worthybitbuilders/squadsense/MyApplication.java
+++ frontend/app/src/main/java/com/worthybitbuilders/squadsense/MyApplication.java
@@ -1,16 +1,10 @@
 package com.worthybitbuilders.squadsense;
 
-import android.Manifest;
 import android.app.Application;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
 
 import com.worthybitbuilders.squadsense.utils.NotificationUtil;
 import com.worthybitbuilders.squadsense.utils.SharedPreferencesManager;
 import com.worthybitbuilders.squadsense.utils.SocketClient;
-
-import pub.devrel.easypermissions.EasyPermissions;
 
 public class MyApplication extends Application {
     @Override
